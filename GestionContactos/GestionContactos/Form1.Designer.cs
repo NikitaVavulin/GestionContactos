1b91626644559b0c4105ca8659634b131b0c32be
MYKYTA VAVULIN <mykvav@alu.edu.gva.es>
2025-01-28 17:08:30 +0100

    He hecho "Añadir Contacto"

---
+++ GestionContactos/GestionContactos/Form1.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace GestionContactos
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnAñadirCont = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnAñadirCont
+            // 
+            this.btnAñadirCont.Location = new System.Drawing.Point(60, 58);
+            this.btnAñadirCont.Name = "btnAñadirCont";
+            this.btnAñadirCont.Size = new System.Drawing.Size(158, 65);
+            this.btnAñadirCont.TabIndex = 0;
+            this.btnAñadirCont.Text = "Añadir contacto";
+            this.btnAñadirCont.UseVisualStyleBackColor = true;
+            this.btnAñadirCont.Click += new System.EventHandler(this.btnAñadirCont_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(549, 450);
+            this.Controls.Add(this.btnAñadirCont);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnAñadirCont;
+    }
+}
+
---
+++ GestionContactos/GestionContactos/Form1.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.VisualBasic;
+
+namespace GestionContactos
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        const int Tam = 10;
+        string[] nombres = new string[Tam];
+        string[] telefonos = new string[Tam];
+        int cont = 0;
+        void añadirContacto(string[] nombres, string[] telefonos, string nombre, string telefono) 
+        {
+            bool stop = false;
+            for (int i = 0; i < nombres.Length || !stop; i++)
+            {
+                if (nombres[i] == null) 
+                {
+                    nombres[i] = nombre;
+                    telefonos[i] = telefono;
+                    stop = true;
+                }
+            }
+        }
+
+
+        
+        bool comprobarNombre(string[] nombres, string nombre) 
+        {
+            bool valido = true;
+            for(int i = 0; i < nombres.Length ; i++)
+            {
+                if (nombres[i] != null) 
+                {
+                    if (nombres[i] == nombre)
+                    {
+                        valido = false;
+                    }
+                }
+            }
+            return valido;
+        }
+
+
+        /*
+        string mostrar(string[] nombres, string[] telefonos) 
+        {
+            string text = "Contactos: \n";
+            for (int i = 0; i < Tam; i++) 
+            {
+                text += nombres[i] + " " + telefonos[i] + "\n";
+            }
+            return text;
+        }
+        */
+
+        private void btnAñadirCont_Click(object sender, EventArgs e)
+        {
+            string nombre = "", telefono = "";
+            nombre = Interaction.InputBox("Nombre: ");
+            while (!comprobarNombre(nombres, nombre))
+            {
+                MessageBox.Show("Nombre repetido");
+                nombre = Interaction.InputBox("Nombre: ");
+            }
+            telefono = Interaction.InputBox("Telefono: ");
+            añadirContacto(nombres, telefonos, nombre, telefono);
+        }
+    }
+}
